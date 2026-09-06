3027db6083608376751f1e34e0c454b9039029f5
mateusedgar431 <mateusedgar365@gmail.com>
2026-09-06 00:12:18 -0300

    Add MaterialService to SERVIÇOS_MESTRES

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import xml.sax.saxutils as saxutils
 app = Flask(__name__)
 
 SERVICOS_MESTRES = {
+    "materialservice": "MaterialService",
     "workspace": "Workspace",
     "lighting": "Lighting",
     "replicatedfirst": "ReplicatedFirst",
