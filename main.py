53c846d3bbb2e7db5b192be06206c28ccdef06fa
mateusedgar431 <mateusedgar365@gmail.com>
2026-09-20 21:46:27 -0300

    Change condition to check for positive rotation ID

--- main.py
+++ main.py
@@ -499,7 +499,7 @@ def prop(t,b,p,n,name):
         for _ in range(n):  
             rot_id = b[p]  
             p += 1  
-            if rot_id != 0:  
+            if rot_id > 0:  
                 vals = floats(b[p:p + 36], 9)  
                 p += 36  
                 r00, r01, r02 = vals[0], vals[1], vals[2]  
