820862b180712a677e319c6722f6379d08352a3a
mateusedgar431 <mateusedgar365@gmail.com>
2026-09-20 22:37:30 -0300

    Update rotation ID assignment in main.py

--- main.py
+++ main.py
@@ -497,7 +497,7 @@ def prop(t,b,p,n,name):
     if t == 0x10:
         rotations = []
         for _ in range(n):
-            rot_id = 0
+            rot_id = b[p]
             p += 1
             if rot_id == 0:
                 vals = floats(b[p:p + 36], 9)
